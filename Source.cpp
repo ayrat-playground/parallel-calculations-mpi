f28ce1c51ea71edab5efab341aaee80f6fcabad4
Ayrat Badykov <ayratin555@gmail.com>
2015-03-18 20:53:30 +0300

    no message

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <io.h>
 #include "mpi.h"
 
 void matrixVectorMultiply(int argc, char* argv[]);
@@ -13,6 +14,9 @@ void dotProduct(int argc, char* argv[]);
 void monteCarloPi(int argc, char* argv[]);
 void averagePositiveCompon(int argc, char* argv[]);
 void vectorInvert(int argc, char* argv[]);
+void matrixVectorMultiplyColumn(int argc, char* argv[]);
+int MatrixNormal(int argc, char **argv);
+int MatrixInverse(int argc, char **argv);
 
 int main(int argc, char* argv[]){
 
@@ -24,7 +28,8 @@ int main(int argc, char* argv[]){
 	//dotProduct(argc, argv);
 	//monteCarloPi(argc, argv);
 	//averagePositiveCompon(argc, argv);
-	vectorInvert(argc, argv);
+	//matrixVectorMultiply(argc, argv);
+	 MatrixNormal(argc, argv);
 
 	
 }
@@ -304,7 +309,7 @@ void averagePositiveCompon(int argc, char* argv[]){
 
 }
 
-/*5.	Умножение матрицы на вектор по столбцам 10 баллов */
+/* Умножение матрицы на вектор по строкам  */
 void matrixVectorMultiply(int argc, char* argv[]){
 
 	double x[100][100],y[100],z[100], TotalProd[100];
@@ -320,10 +325,10 @@ void matrixVectorMultiply(int argc, char* argv[]){
 		int min = 0;
 		for (int i = 0; i < N; i++){
 			z[i] = 0;
-			y[i] = min + (rand() % (int)(max - min + 1));
+			y[i] = 1;
 			for (int j = 0; j < N; j++){
 
-				x[i][j] = min + (rand() % (int)(max - min + 1));
+				x[i][j] = 1;
 			}
 		}
 	}
@@ -391,5 +396,198 @@ void vectorInvert(int argc, char* argv[]){
 	system("pause");
 
 }
+/* умножение матрицы на вектор по столбцам*/
+void matrixVectorMultiplyColumn(int argc, char* argv[]){
+
+	double x[100][100], y[100], z[100],d[100], TotalRes[100], res[100];
+	int ProcRank, ProcNum, N = 100;
+	MPI_Status Status;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+	if (ProcRank == 0) {
+		int max = 1000;
+		int min = 0;
+		for (int i = 0; i < N; i++){
+			z[i] = 0;
+			y[i] = 1;
+			for (int j = 0; j < N; j++){
+
+				x[i][j]=1;
+			}
+		}
+	}
+
+	MPI_Bcast(x, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(y, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(z, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	int k = N / ProcNum;
+	int i1 = k * ProcRank;
+	int i2 = k * (ProcRank + 1);
+	if (ProcRank == ProcNum - 1) i2 = N;
+	for (int i = i1; i < i2; i++)
+		for (int j = 0; j<N; j++){
+			z[j] = +y[j] * x[j][i];
+
+		}
+	MPI_Reduce(&z, &TotalRes, N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
 
+	MPI_Finalize();
+	for (int i = 0; i < N; i++){
+		printf("\nProds[%i] = %10.2f", i, TotalRes[i]);
+	}
+	system("pause");
+
+
+}
+
+int MatrixNormal(int argc, char **argv) {
+	int myrank, total, N_PER_PROC=2;
+
+	double *A, *B, *C;	// Используются только в root
+
+	double *a, *b, *c;	// Лента матрицы [mxn], вектор [n], рез-т [m]
+	int n, m;
+	int i, j;
+	int intBuf[2];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &total);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	printf("Total=%d, rank=%d\n", total, myrank);
+
+	if (!myrank) {	// Подготовка исх. данных (только root)
+		n = N_PER_PROC * total;
+		A = (double *)malloc(sizeof(double)*n*n);
+		B = (double *)malloc(sizeof(double)*n);
+		C = (double *)malloc(sizeof(double)*n);
+		for (i = 0; i<n; i++) {
+			B[i] = (double)i;
+			for (j = 0; j<n; j++)
+				A[i*n + j] = (double)(i + j);
+		};
+	};
+
+	if (!myrank) {
+		intBuf[0] = n;
+		intBuf[1] = N_PER_PROC;
+	};
+	MPI_Bcast((void *)intBuf, 2, MPI_INT, 0, MPI_COMM_WORLD);
+	n = intBuf[0];
+	m = intBuf[1];
+
+	a = (double *)malloc(sizeof(double)*n*m);
+	b = (double *)malloc(sizeof(double)*n);
+	c = (double *)malloc(sizeof(double)*m);
+
+	if (!myrank) {	// Лишнее действие, B не нужен
+		for (int i = 0; i < n; i++){
+			b[i] = B[i];
+		}
+	};
+	MPI_Bcast((void *)b, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	MPI_Scatter((void *)A, n*m, MPI_DOUBLE,
+		(void *)a, n*m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	for (i = 0; i<m; i++) {
+		c[i] = 0;
+		for (j = 0; j<n; j++)
+			c[i] += a[n*i + j] * b[j];
+	};
+
+	MPI_Gather((void *)c, m, MPI_DOUBLE,
+		(void *)C, m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	if (!myrank)
+		for (i = 0; i<n; i++)
+			printf("%g\n", C[i]);
+
+	MPI_Finalize();
+	system("pause");
+	exit(0);
+}
+
+int MatrixInverse(int argc, char **argv) {
+	int myrank, total, N_PER_PROC = 2;
+
+	double *A, *B, *C;	// Используются только в root
+
+	double *a, *b, *c;	// Лента матрицы [mxn], вектор [n], рез-т [m]
+	int n, m;
+	int i, j;
+	int intBuf[2];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &total);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	printf("Total=%d, rank=%d\n", total, myrank);
+
+	if (!myrank) {	// Подготовка исх. данных (только root)
+		n = N_PER_PROC * total;
+		A = (double *)malloc(sizeof(double)*n*n);
+		B = (double *)malloc(sizeof(double)*n);
+		C = (double *)malloc(sizeof(double)*n);
+		for (i = 0; i<n; i++) {
+			B[i] = (double)i;
+			for (j = 0; j<n; j++)
+				A[i*n + j] = (double)(i + j);
+		};
+		for (i = 0; i<n; i++) {
+			B[i] = (double)i;
+			for (j = 0; j<n; j++)
+				A[i*n + j] = (double)(i + j);
+		};
+		for ( i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				double tmp = A[i*n + j];
+				A[i*n + j] = A[j*n + i];
+				A[j*n + i] = tmp;
+			}
+		}
+	};
+
+	if (!myrank) {
+		intBuf[0] = n;
+		intBuf[1] = N_PER_PROC;
+	};
+	MPI_Bcast((void *)intBuf, 2, MPI_INT, 0, MPI_COMM_WORLD);
+	n = intBuf[0];
+	m = intBuf[1];
+
+	a = (double *)malloc(sizeof(double)*n*m);
+	b = (double *)malloc(sizeof(double)*n);
+	c = (double *)malloc(sizeof(double)*m);
+
+	if (!myrank) {	// Лишнее действие, B не нужен
+		for (int i = 0; i < n; i++){
+			b[i] = B[i];
+		}
+	};
+	MPI_Bcast((void *)b, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	MPI_Scatter((void *)A, n*m, MPI_DOUBLE,
+		(void *)a, n*m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	for (i = 0; i<m; i++) {
+		for (j = 0; j<n; j++)
+			c[j] = a[n*i + j] * b[j];
+	};
+
+	MPI_Gather((void *)c, m, MPI_DOUBLE,
+		(void *)C, m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	if (!myrank)
+		for (i = 0; i<n; i++)
+			printf("%g\n", C[i]);
+
+	MPI_Finalize();
+	system("pause");
+	exit(0);
+}
